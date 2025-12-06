4fef138b6760450bd62bd8c6be5201397b648e53
Emil J. Tywoniak <emil@tywoniak.eu>
2025-12-06 16:49:30 +0100

    day 2

--- src/main.zig
+++ src/main.zig
@@ -47,34 +47,40 @@ fn mode1_2(fr: *std.Io.Reader) !i64 {
     return pings;
 }
 
-fn log10(num: usize) usize {
+fn digit_len(num: usize) usize {
     var tmp = num;
     var cnt: usize = 0;
     while (true) {
-        if (tmp == 0) return cnt;
+        if (tmp == 0) break;
         tmp /= 10;
         cnt += 1;
     }
+    if (cnt == 0)
+        cnt = 1;
+    return cnt;
 }
-test "log10" {
-    try std.testing.expectEqual(log10(0), @as(usize, 1));
-    try std.testing.expectEqual(log10(1), @as(usize, 1));
-    try std.testing.expectEqual(log10(1000), @as(usize, 3));
+
+test "digit_len" {
+    try std.testing.expectEqual(1, digit_len(0));
+    try std.testing.expectEqual(1, digit_len(1));
+    try std.testing.expectEqual(4, digit_len(1000));
+    try std.testing.expectEqual(4, digit_len(2121));
 }
+
 const DigitIterator = struct {
     remains_length: usize = 0,
     remains: usize = 0,
-    fn next(self: *DigitIterator) ?[]const u8 {
+    fn next(self: *DigitIterator) ?u8 {
         if (self.remains_length == 0)
             return null;
-        const ret = self.remains % 10;
+        const ret: u8 = @intCast(self.remains % 10);
         self.remains = self.remains / 10;
         self.remains_length -= 1;
         return ret;
     }
     pub fn init(num: usize) DigitIterator {
         return DigitIterator{
-            .remains_length = log10(num) + 1,
+            .remains_length = digit_len(num),
             .remains = num,
         };
     }
@@ -85,10 +91,10 @@ fn Square(comptime T: type) type {
         const Self = @This();
         dim: usize,
         data: []T,
-        pub fn init(d: usize, alloc: std.mem.Allocator) Self {
+        pub fn init(d: usize, alloc: std.mem.Allocator) !Self {
             return Self{
                 .dim = d,
-                .data = (try alloc.alloc(usize, d * d)).?,
+                .data = try alloc.alloc(T, d * d),
             };
         }
         pub fn set(self: Self, idx1: usize, idx2: usize, val: T) void {
@@ -100,52 +106,97 @@ fn Square(comptime T: type) type {
     };
 }
 
-fn goofy(num: usize, alloc: std.mem.Allocator) bool {
-    const digits = DigitIterator.init(num);
-    const digits2 = digits;
+fn goofy(num: usize, alloc: std.mem.Allocator, second_lvl: bool) !bool {
+    var digits = DigitIterator.init(num);
+    var digits2 = digits;
     const length = digits.remains_length;
-    const patterns = Square(?u8).init(length, alloc);
+    const patterns = try Square(?u8).init(length, alloc);
+    defer alloc.free(patterns.data);
     for (0..length) |idx1| {
         for (0..length) |idx2| {
             patterns.set(idx1, idx2, null);
         }
     }
     // const patterns = [length][length]?u8{} ** null;
-    var correct = alloc.alloc(bool, length);
+    var correct = try alloc.alloc(bool, length);
+    std.debug.print("{} num\n", .{num});
+    defer alloc.free(correct);
     // const correct = [length]bool{} ** true;
-    for (digits, 0..) |digit, digit_idx| {
-        for (0..length) |pat_len| {
-            patterns.set(pat_len, digit_idx, if (digit_idx < pat_len)
+    for (0..length) |pat_idx| {
+        correct[pat_idx] = true;
+    }
+    var digit_idx: usize = 0;
+    while (digits.next()) |digit| {
+        for (1..length + 1) |pat_len| {
+            patterns.set(pat_len - 1, digit_idx, if (digit_idx < pat_len)
                 digit
             else
-                patterns.get(pat_len, digit_idx % pat_len));
+                patterns.get(pat_len - 1, digit_idx % pat_len));
         }
+        digit_idx += 1;
     }
-    for (digits2, 0..) |digit, digit_idx| {
-        for (0..length) |pat_len| {
-            if (patterns[pat_len][digit_idx] != digit)
-                correct[digit_idx] = false;
+    digit_idx = 0;
+    while (digits2.next()) |digit| {
+        for (1..length + 1) |pat_len| {
+            // std.debug.print("sniff {} {} = {} , {}\n", .{ pat_len - 1, digit_idx, patterns.get(pat_len - 1, digit_idx).?, digit });
+            if (patterns.get(pat_len - 1, digit_idx) != digit) {
+                // std.debug.print("false {}\n", .{pat_len - 1});
+                correct[pat_len - 1] = false;
+            }
         }
+        digit_idx += 1;
     }
-    for (0..length) |pat_len| {
-        if (correct[pat_len])
+    // for (correct) |c| {
+    //     std.debug.print("correct {}\n", .{c});
+    // }
+    for (0..length - 1) |pat_idx| {
+        const pat_len = pat_idx + 1;
+        if (!second_lvl and correct[pat_idx] and length % pat_len == 0 and length / pat_len == 2) {
+            // std.debug.print("win {}\n", .{pat_len});
             return true;
+        }
+        if (second_lvl and correct[pat_idx] and length % pat_len == 0) {
+            // std.debug.print("win {}\n", .{pat_len});
+            return true;
+        }
     }
     return false;
 }
-fn mode2_1(fr: *std.Io.Reader, alloc: std.mem.Allocator) !i64 {
+fn goofy_in_range(alloc: std.mem.Allocator, start: usize, end: usize, second_lvl: bool) !i64 {
+    std.debug.print("in range {} {}?\n", .{ start, end });
     var count: usize = 0;
+    var sum: usize = 0;
+    for (start..end + 1) |num| {
+        if (try goofy(num, alloc, second_lvl)) {
+            // std.debug.print("goofy {}\n", .{num});
+            count += 1;
+            sum += num;
+        }
+    }
+    std.debug.print("in range {} sum {}\n", .{ count, sum });
+    return @intCast(sum);
+}
+
+fn mode2(fr: *std.Io.Reader, alloc: std.mem.Allocator, second_lvl: bool) !i64 {
+    var count: i64 = 0;
     while (true) {
         const start_s = fr.takeDelimiterExclusive('-') catch break;
+        _ = try fr.takeByte();
         const start = try std.fmt.parseInt(usize, start_s, 10);
         const end_s = try fr.takeDelimiterExclusive(',');
         const end = std.fmt.parseInt(usize, end_s, 10) catch break;
-        for (start..end + 1) |num| {
-            if (goofy(num, alloc))
-                count += num;
-        }
+        count += @intCast(try goofy_in_range(alloc, start, end, second_lvl));
+        _ = fr.takeByte() catch break;
     }
-    return @intCast(count);
+    return count;
+}
+
+test "goofy" {
+    const alloc = std.testing.allocator;
+    // const inp: []u8 = "1000-1111";
+    try std.testing.expectEqual(true, goofy(2121, alloc));
+    try std.testing.expectEqual(false, goofy(1091, alloc));
+    try std.testing.expectEqual(2, goofy_in_range(alloc, 1000, 1111));
 }
 
 pub fn main() !void {
@@ -172,9 +223,9 @@ pub fn main() !void {
         else if (day == 1 and mode == 2)
             try mode1_2(fr)
         else if (day == 2 and mode == 1)
-            try mode2_1(fr, gpa)
-            // else if (day == 2 and mode == 2)
-            //     try mode2_2(fr)
+            try mode2(fr, gpa, false)
+        else if (day == 2 and mode == 2)
+            try mode2(fr, gpa, true)
         else {
             unreachable;
         };
